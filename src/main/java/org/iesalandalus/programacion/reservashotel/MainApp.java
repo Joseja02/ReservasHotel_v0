718180431f280fb7932ac3ef6e9c7221b7cb0af4
Jose Javier Sierra <jsieber1908@gmail.com>
2024-01-22 19:53:44 +0100

    Clase Huespedes creada, pendiente de completar. Hago Commit and Push a modo de copia de seguridad debido a un problema con mi PC.

--- src/main/java/org/iesalandalus/programacion/reservashotel/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reservashotel/MainApp.java
@@ -2,8 +2,9 @@ package org.iesalandalus.programacion.reservashotel;
 
 
 public class MainApp {
-    
+    public static int CAPACIDAD = 4;
     public static void main(String[] args) {
+
         System.out.println("Hasta luego!!!!");
     }
 }
---
+++ src/main/java/org/iesalandalus/programacion/reservashotel/negocio/Huespedes.java
@@ -0,0 +1,128 @@
+package org.iesalandalus.programacion.reservashotel.negocio;
+
+import org.iesalandalus.programacion.reservashotel.dominio.Huesped;
+
+import javax.naming.OperationNotSupportedException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Huespedes {
+    private int capacidad;
+    private int tamano;
+    private Huesped[] huespedes;
+
+    public Huespedes (int capacidad){
+
+        if (capacidad <= 0){
+            throw new IllegalArgumentException("ERROR: La capacidad debe ser mayor que cero.");
+        }
+
+        this.capacidad = capacidad;
+        huespedes = new Huesped[this.capacidad];
+        tamano = 0;
+    }
+
+    public Huesped[] get() {
+        return copiaProfundaHuespedes();
+    }
+
+    private Huesped[] copiaProfundaHuespedes(){
+
+        Huesped[] copiaHuespedes = new Huesped[huespedes.length];
+
+        for (int i = 0; i < huespedes.length; i++){
+            copiaHuespedes[i] = new Huesped(huespedes[i]);
+        }
+        return copiaHuespedes;
+    }
+    public int getCapacidad() {
+        return capacidad;
+    }
+
+    public int getTamano() {
+        return tamano;
+    }
+
+    public void insertar (Huesped huesped) throws OperationNotSupportedException {
+
+        if (huesped == null){
+            throw new NullPointerException("ERROR: No se puede insertar un huésped nulo.");
+        }
+
+        tamano = huespedes.length;
+
+        for (int i = 0; i < tamano; i++){
+            if (huesped.equals(huespedes[i])){
+                throw new OperationNotSupportedException("ERROR: Ya existe un huésped con ese dni.");
+            }
+        }
+
+        if(capacidadSuperada(tamano)){
+            throw new OperationNotSupportedException("ERROR: No se aceptan más huéspedes.");
+        }
+
+        Huesped [] nuevoArray = new Huesped[tamano + 1];
+
+        for (int i = 0; i < tamano; i++) {
+            nuevoArray[i] = huespedes[i];
+        }
+        nuevoArray[nuevoArray.length - 1] = huesped;
+
+        huespedes = nuevoArray;
+        tamano = huespedes.length;
+    }
+    private int buscarIndice (Huesped huesped){
+
+        for (int i = 0; i < tamano; i++) {
+            if (huespedes[i].equals(huesped)){
+                return i;
+            }
+        }
+        return -1;
+    }
+    private boolean tamanoSuperado (int indice){
+        return indice == tamano-1;
+    }
+    private boolean capacidadSuperada (int indice){
+        return indice > capacidad;
+    }
+
+    public Huesped buscar (Huesped huesped){
+
+        if (huesped == null){
+            throw new NullPointerException("El huésped devuelto no es el que debería ser.");
+        }
+
+        int indice = buscarIndice(huesped);
+        if(indice == -1){
+            return null;
+        }
+
+        return huespedes[indice];
+    }
+
+    public void borrar (Huesped huesped) throws OperationNotSupportedException {
+
+        if (huesped == null){
+            throw new NullPointerException("ERROR: No se puede borrar un huésped nulo.");
+        }
+
+        int indice = buscarIndice(huesped);
+        if (indice < 0){
+            throw new NullPointerException("ERROR: No existe ningún huésped como el indicado.");
+        }
+
+        desplazarUnaPosicionHaciaLaIzquierda(indice);
+    }
+    private void desplazarUnaPosicionHaciaLaIzquierda (int indice){
+        Huesped[] nuevoArray = new Huesped[tamano - 1];
+        System.arraycopy(huespedes, 0, nuevoArray, 0, indice);
+        if (!tamanoSuperado(indice)) {
+            System.arraycopy(huespedes, indice + 1, nuevoArray, indice, huespedes.length - indice - 1);
+        }
+        huespedes = nuevoArray;
+    }
+
+// if (huespedes.length - 1 != indice)
+}
